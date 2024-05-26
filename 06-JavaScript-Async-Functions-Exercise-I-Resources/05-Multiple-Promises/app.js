c0eec9fa739b6c723594445bac92d5a6faef37d6
Denis Atanassov <dennis.atanasoff@gmail.com>
2024-05-26 23:04:47 +0300

    second commit

--- 06-JavaScript-Async-Functions-Exercise-I-Resources/05-Multiple-Promises/app.js
+++ 06-JavaScript-Async-Functions-Exercise-I-Resources/05-Multiple-Promises/app.js
@@ -1,3 +1,27 @@
-function multiplePromises() {
-      // TODO...
+async function multiplePromises() {
+      const promise1 = new Promise (function(resolve, reject){
+            setTimeout(function(){
+                  resolve('Resolved after 1 second!');
+            }, 1000)
+      });
+
+      const promise2 = new Promise (function(resolve, reject){
+            setTimeout(function(){
+                  resolve('Resolved after 2 seconds!');
+            }, 2000)
+      });
+
+      const promise3 = new Promise (function(resolve, reject){
+            setTimeout(function(){
+                  reject('Rejected after 3 seconds!');
+            }, 3000)
+      });
+
+      // Promise.allSettled([promise1, promise2, promise3])
+      // .then(result => {
+      //       result.forEach(result => console.log(result.status, result.value || result.reason));
+      // });
+
+      const results = await Promise.allSettled([promise1, promise2, promise3]);
+      results.forEach(result => console.log(result.status, result.value || result.reason));
 }
--- 06-JavaScript-Async-Functions-Exercise-I-Resources/06-Retrying-a-Failed-Promise/app.js
+++ 06-JavaScript-Async-Functions-Exercise-I-Resources/06-Retrying-a-Failed-Promise/app.js
@@ -1,3 +1,21 @@
 async function retryPromise(promiseFunc, retries = 3) {
-  // TODO...
+  for (let i = 0; i < retries; i++){
+    try {
+      const result = await promiseFunc();
+      return result;
+    } catch (error) {
+      if (i === retries - 1) throw error;
+    }
+  }
+}
+
+const unstablePromise = () => new Promise((resolve, reject) => {
+    Math.random() > 0.5 ? resolve('Success!') : reject(new Error('Faield!'));
+});
+
+function startRetry()
+{
+  retryPromise(unstablePromise)
+  .then(result => console.log(`Result: ${result}`))
+  .catch(error => console.log(`Error: ${error.message}`))
 }
