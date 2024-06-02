01931dc9c62930cc7f6a93835f659580f81d4b42
Denis Atanassov <dennis.atanasoff@gmail.com>
2024-06-02 18:42:39 +0300

    second commit

--- 09-DOM-and-Events-Exercise-Resources/01-Subtraction/subtract.js
+++ 09-DOM-and-Events-Exercise-Resources/01-Subtraction/subtract.js
@@ -1,3 +1,11 @@
 function subtract() {
-    // TODO...
+    const firstNumber = document.getElementById("firstNumber");
+    const secondNumber = document.getElementById("secondNumber");
+    const resultDiv = document.getElementById("result");
+    
+    let firstValue = Number(firstNumber.value);
+    let secondValue = Number(secondNumber.value);
+    let finalResult = firstValue - secondValue;
+
+    resultDiv.textContent = finalResult;
 } 
--- 09-DOM-and-Events-Exercise-Resources/02-Pascal-or-Camel-Case/solution.js
+++ 09-DOM-and-Events-Exercise-Resources/02-Pascal-or-Camel-Case/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  // TODO...
+  let input = document.getElementById('text').value.toLowerCase();
+  let convention = document.getElementById('naming-convention').value;
+  let result = document.getElementById('result');
+
+  let arrayInput = input.split(' ');
+  let resultText = '';
+
+  if(convention == "Camel Case"){
+    resultText += arrayInput[0];
+    for(let i = 1; i < arrayInput.length; i++){
+      resultText += arrayInput[i][0].toUpperCase() + arrayInput[i].slice(1, arrayInput[i].length);
+    }
+
+    result.textContent = resultText;
+  }
+  else if(convention == "Pascal Case"){
+    resultText += arrayInput[0][0].toUpperCase() + arrayInput[0].slice(1, arrayInput[0].length);
+    for (let i = 1; i < arrayInput.length; i++) {
+      resultText += arrayInput[i][0].toUpperCase() + arrayInput[i].slice(1, arrayInput[i].length);
+    }
+
+    result.textContent = resultText;
+  }
+  else{
+    result.textContent = "Error!"
+  }
 }
