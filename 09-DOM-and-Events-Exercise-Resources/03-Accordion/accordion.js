b2f0dd4fcf690f1810f3a4cad41e7821b25ff1d5
Denis Atanassov <dennis.atanasoff@gmail.com>
2024-06-02 18:56:17 +0300

    third

--- 09-DOM-and-Events-Exercise-Resources/03-Accordion/accordion.js
+++ 09-DOM-and-Events-Exercise-Resources/03-Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    // TODO...
+    let textDiv = document.getElementById('extra');
+    let button = document.getElementsByClassName('button')[0];
+
+    if(button.textContent == 'More'){
+        textDiv.style.display = 'block';
+        button.textContent = 'Less';
+    }
+    else{
+        textDiv.style.display = 'none';
+        button.textContent = 'More';
+    }
 }
